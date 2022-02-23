7e3a1f21bc67e116b2a06fd2e6264fb651bf2a03
1amdonut <joshchia1227@gmail.com>
2022-02-23 10:57:21 +0800

    init

---
+++ Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace myAspDotnet
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            string data = Environment.GetEnvironmentVariable("EMAIL_PASSWORD");
+            if (data == null) {
+                Label1.Text = "null"; 
+            } else { 
+                Label1.Text = data; 
+            }
+        }
+    }
+}
